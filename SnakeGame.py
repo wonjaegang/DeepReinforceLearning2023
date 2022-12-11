034a9846501889384569aefec0708fab2fc77c38
Wonjaegang <robinsam@naver.com>
2022-12-11 13:27:37 +0900

    Implement basic snake-game

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,5 +1,7 @@
 import pygame
 import time
+import itertools
+import random
 from collections import deque
 
 
@@ -16,36 +18,35 @@ GRID_NUM = 50
 GRID_SIZE = int(XY_MAX / GRID_NUM)
 screen = pygame.display.set_mode((XY_MAX, XY_MAX))
 clock = pygame.time.Clock()
-rate = 10
+rate = 20
 
 
-class Map:
+class Food:
     def __init__(self):
-        pass
+        self.location = []
+        self.initialize_location()
 
+    def displayGUI(self):
+        pygame.draw.rect(screen, RED, (GRID_SIZE * self.location[0], GRID_SIZE * self.location[1], GRID_SIZE, GRID_SIZE))
+
+    def initialize_location(self):
+        self.location = [random.choice(range(GRID_NUM)), random.choice(range(GRID_NUM))]
 
 
 class Snake:
     def __init__(self):
-        self.direction = deque([4, 4])  # 1, 2, 3, 4 as top, left, bottom, right
-        self.location = [[9, 20], [10, 20]]  # x, y, 좌상단이 0,0
+        self.direction = 4  # 1, 2, 3, 4 as top, left, bottom, right
+        self.location = deque([[9, 20], [10, 20]])  # x, y, 좌상단이 0,0
+        self.tail_location = self.location[0]  #
 
-    def move(self, direction):
-        self.set_direction(direction)
-        self.move_by_direction()
+    def move(self):
+        dx = (1 - self.direction % 2) * (self.direction - 3)
+        dy = (self.direction % 2) * (self.direction - 2)
+        x = self.location[-1][0] + dx
+        y = self.location[-1][1] + dy
 
-    def set_direction(self, direction):
-        if not direction:  # 입력이 없으면
-            direction = self.direction[-1]
-        self.direction.popleft()
-        self.direction.append(direction)
-
-    def move_by_direction(self):
-        for i, direction in enumerate(self.direction):
-            dx = (1 - direction % 2) * (direction - 3)
-            dy = (direction % 2) * (direction - 2)
-            self.location[i][0] += dx
-            self.location[i][1] += dy
+        self.location.append([x, y])
+        self.tail_location = self.location.popleft()
 
     def check_collision(self):
         if not 0 <= self.location[-1][0] < GRID_NUM:
@@ -53,14 +54,13 @@ class Snake:
         elif not 0 <= self.location[-1][1] < GRID_NUM:
             return True
         else:
-            if self.location[-1] in self.location[:-1]:
+            if self.location[-1] in itertools.islice(self.location, 0, len(self.location) - 1):
                 return True
             else:
                 return False
 
-    def grow(self):  # 움직이기 전, 성장 후 움직임
-        pass
-
+    def grow(self):
+        self.location.appendleft(self.tail_location)
 
     def displayGUI(self):
         for location in self.location:
@@ -69,33 +69,51 @@ class Snake:
 
 def main():
     snake = Snake()
+    food = Food()
+    count = 0
     while True:
-        input_direction = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return 0
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    input_direction = 1
+                    if not snake.direction == 3:
+                        snake.direction = 1
+                        break
                 elif event.key == pygame.K_LEFT:
-                    input_direction = 2
+                    if not snake.direction == 4:
+                        snake.direction = 2
+                        break
                 elif event.key == pygame.K_DOWN:
-                    input_direction = 3
+                    if not snake.direction == 1:
+                        snake.direction = 3
+                        break
                 elif event.key == pygame.K_RIGHT:
-                    input_direction = 4
+                    if not snake.direction == 2:
+                        snake.direction = 4
+                        break
 
         screen.fill(WHITE)
 
-        snake.move(input_direction)
+        snake.move()
+
+        # snake 성장 여부 판정
+        if snake.location[-1] == food.location:
+            snake.grow()
+            food.initialize_location()
+
+        # snake 충돌 여부 판정
         if snake.check_collision():
-            time.sleep(2)
+            time.sleep(1)
             main()
             break
 
+        food.displayGUI()
         snake.displayGUI()
         pygame.display.flip()
         clock.tick(rate)
+        count += 1
 
 
 if __name__ == '__main__':
